54f9b269b66ecffe84c5d1b050c4452ac8d34078
Makoto8247 <48078578+Makoto8247@users.noreply.github.com>
2025-05-11 09:49:44 +0900

    ジャイロセンサーのライブラリを変更した

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,58 @@
 #include <Arduino.h>
+#include <ArduinoLowPower.h>
 #include <Wire.h>
-#include <ICM20689.h> // Gyro and accelerometer library
 #include <Adafruit_MPR121.h> // Capacitive touch library
-#include "ArduinoLowPower.h"
+#include <LSM6DS3.h> // Gyro and accelerometer library
 
 #define READV_PIN PA2
 #define POWER_BUTTON_PIN PD0 // Power button pin
 #define LED_PIN D1
 #define SPI_CS_PIN PD2 // Chip select pin for SPI
+
+
+// BLE
+#define BLE_NAME "BLE_MG24_MOUSE"
+
+// Mouse
+struct mouseData {
+  int8_t delta_x;
+  int8_t delta_y;
+  uint8_t buttons;
+};
+static mouseData report_mouse;
+static uint8_t report_mouse_array[3];
+
+static uint8_t connection_handle = SL_BT_INVALID_CONNECTION_HANDLE;
+static uint32_t bonding_handle = SL_BT_INVALID_BONDING_HANDLE;
+static uint16_t hid_report;
+
+static void ble_initialize_gatt_db() {
+  sl_status_t status;
+  uint16_t gattdb_session_id;
+  uint16_t service;
+  uint16_t characteristic;
+  uint16_t descriptor;
+
+  // Create a new GATT database session
+  status = sl_bt_gattdb_new_session(&gattdb_session_id);
+  app_assert_status(status);
+
+  // Device Information Service
+  sl_bt_uuid_16_t device_name_uuid = { .data = {0x2A, 0x00} };
+  status = sl_bt_gattdb_add_uuid16_characteristic(gattdb_session_id,
+                                                  service,
+                                                  (SL_BT_GATTDB_CHARACTERISTIC_READ | SL_BT_GATTDB_CHARACTERISTIC_WRITE),
+                                                  0,
+                                                  0,
+                                                  device_name_uuid,
+                                                  sl_bt_gattdb_fixed_length_value,
+                                                  strlen(BLE_NAME),
+                                                  strlen(BLE_NAME),
+                                                  (uint8_t *)BLE_NAME,
+                                                  &characteristic);
+  app_assert_status(status);
+}
+
 /*
  * ボタンマトリックス割り当て表
  * 
@@ -40,7 +85,8 @@ void initializeMatrixPins() {
   }
 }
 
-ICM20689 gyroIMU(SPI, SPI_CS_PIN);
+#define LSM6DS3_ADDRESS 0x6A // I2C address for LSM6DS3
+LSM6DS3 gyroIMU((unsigned char)I2C_MODE, (unsigned char)LSM6DS3_ADDRESS);
 Adafruit_MPR121 touchSensor = Adafruit_MPR121();
 
 void readVoltage() {
@@ -86,7 +132,18 @@ void setup() {
 
 void loop() {
   readVoltage();
-  Serial.println(gyroIMU.readSensor());
+  Serial.print("Gyro AccelX: ");
+  Serial.println(gyroIMU.readFloatAccelX());
+  Serial.print("Gyro AccelY: ");
+  Serial.println(gyroIMU.readFloatAccelY());
+  Serial.print("Gyro AccelZ: ");
+  Serial.println(gyroIMU.readFloatAccelZ());
+  Serial.print("Gyro GyroX: ");
+  Serial.println(gyroIMU.readFloatGyroX());
+  Serial.print("Gyro GyroY: ");
+  Serial.println(gyroIMU.readFloatGyroY());
+  Serial.print("Gyro GyroZ: ");
+  Serial.println(gyroIMU.readFloatGyroZ());
 
 //  if(checkForDeepSleep()) {
 //    LowPower.deepSleep();
