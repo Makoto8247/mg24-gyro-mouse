83d8adef0464a6bd96ed7c87461d3cd2a29f76d6
Makoto8247 <48078578+Makoto8247@users.noreply.github.com>
2025-05-18 11:47:19 +0900

    I2Cのハードウェア的バグがあり、ソフトウェアI2Cで代用

---
+++ src/MPR121.cpp
@@ -0,0 +1,176 @@
+#include "MPR121.h"
+
+MPR121::MPR121() {}
+
+bool MPR121::begin(uint8_t i2caddr, uint8_t sda_pin, uint8_t scl_pin,
+                   uint8_t touch_threshold, uint8_t release_threshold) {
+
+    if (softWire) {
+        delete softWire;
+    }
+    softWire = new SoftWire(sda_pin, scl_pin);
+
+    // ソフトウェアI2C用のバッファを割り当て
+    Serial.println("Allocating buffers for SoftWire");
+    static uint8_t rxBuffer[32];
+    static uint8_t txBuffer[32];
+    softWire->setRxBuffer(rxBuffer, sizeof(rxBuffer));
+    softWire->setTxBuffer(txBuffer, sizeof(txBuffer));
+
+    Serial.println("Initializing SoftWire");
+    softWire->begin();
+    // クロックを低速に設定（安定性のため）
+    softWire->setClock(100000);
+    
+    // 通信テスト
+    Serial.print("Testing I2C address: 0x");
+    Serial.println(i2caddr, HEX);
+    softWire->beginTransmission(i2caddr);
+    uint8_t err = softWire->endTransmission();
+    if (err != 0) {
+        return false;
+    }
+    
+    _i2caddr = i2caddr;
+
+    // ソフトリセット
+    Serial.println("Soft reset");
+    writeRegister(MPR121_SOFTRESET, 0x63);
+    delay(10); // 1msから10msに延長
+    
+    // 動作設定レジスタをゼロに（スタンバイモード）
+    Serial.println("Setting ECR to 0");
+    writeRegister(MPR121_ECR, 0x0);
+
+    // CONFIG2レジスタで動作確認
+    Serial.println("Checking CONFIG2");
+    uint8_t c = readRegister8(MPR121_CONFIG2);
+    if (c != 0x24) {
+        return false;
+    }
+
+    // タッチと離れた時のしきい値を設定
+    Serial.println("Setting thresholds");
+    setThresholds(touch_threshold, release_threshold);
+    
+    // デジタルフィルタ設定
+    Serial.println("Setting digital filter");
+    writeRegister(MPR121_MHDR, 0x01);
+    writeRegister(MPR121_NHDR, 0x01);
+    writeRegister(MPR121_NCLR, 0x0E);
+    writeRegister(MPR121_FDLR, 0x00);
+
+    writeRegister(MPR121_MHDF, 0x01);
+    writeRegister(MPR121_NHDF, 0x05);
+    writeRegister(MPR121_NCLF, 0x01);
+    writeRegister(MPR121_FDLF, 0x00);
+
+    writeRegister(MPR121_NHDT, 0x00);
+    writeRegister(MPR121_NCLT, 0x00);
+    writeRegister(MPR121_FDLT, 0x00);
+
+    // その他の設定
+    writeRegister(MPR121_DEBOUNCE, 0);
+    writeRegister(MPR121_CONFIG1, 0x10); // 16uA充電電流
+    writeRegister(MPR121_CONFIG2, 0x20); // 0.5uSエンコード、1ms周期
+
+    // 電極を有効化してMPR121を起動
+    Serial.println("Setting ECR");
+    byte ECR_SETTING = B10000000 + 12; // ベースライントラッキング用5ビット + 近接検知無効 + 電極12個
+    writeRegister(MPR121_ECR, ECR_SETTING); 
+
+    return true;
+}
+
+// setThresholds関数の実装
+void MPR121::setThresholds(uint8_t touch, uint8_t release) {
+  // すべての電極に同じしきい値を設定
+  for (uint8_t i = 0; i < 12; i++) {
+    writeRegister(MPR121_TOUCHTH_0 + 2 * i, touch);
+    writeRegister(MPR121_RELEASETH_0 + 2 * i, release);
+  }
+}
+
+// タッチ状態の取得
+uint16_t MPR121::touched() {
+  uint16_t t = readRegister16(MPR121_TOUCHSTATUS_L);
+  return t & 0x0FFF;
+}
+
+// フィルタ処理されたデータの取得
+uint16_t MPR121::filteredData(uint8_t t) {
+  if (t > 12)
+    return 0;
+  return readRegister16(MPR121_FILTDATA_0L + t * 2);
+}
+
+// ベースラインデータの取得
+uint16_t MPR121::baselineData(uint8_t t) {
+  if (t > 12)
+    return 0;
+  uint16_t bl = readRegister8(MPR121_BASELINE_0 + t);
+  return (bl << 2);
+}
+
+// 8ビットレジスタの読み取り
+uint8_t MPR121::readRegister8(uint8_t reg) {
+  softWire->beginTransmission(_i2caddr);
+  softWire->write(reg);
+  softWire->endTransmission(false); // リピートスタート
+
+  uint8_t value = 0;
+  if (softWire->requestFrom(_i2caddr, (uint8_t)1) == 1) {
+    value = softWire->read();
+  }
+  
+  return value;
+}
+
+// 16ビットレジスタの読み取り (LSBファースト)
+uint16_t MPR121::readRegister16(uint8_t reg) {
+  uint16_t value = 0;
+  
+  softWire->beginTransmission(_i2caddr);
+  softWire->write(reg);
+  softWire->endTransmission(false); // リピートスタート
+  
+  if (softWire->requestFrom(_i2caddr, (uint8_t)2) == 2) {
+    value = softWire->read();       // 下位バイト
+    value |= ((uint16_t)softWire->read() << 8); // 上位バイト
+  }
+  
+  return value;
+}
+
+// レジスタへの書き込み
+void MPR121::writeRegister(uint8_t reg, uint8_t value) {
+  // MPR121はほとんどのレジスタに書き込むためにはスタンバイモードにする必要がある
+  bool stop_required = true;
+  uint8_t ecr_backup = 0;
+  
+  // ECRレジスタの現在の値を取得
+  if ((reg == MPR121_ECR) || ((0x73 <= reg) && (reg <= 0x7A))) {
+    stop_required = false;
+  } else {
+    ecr_backup = readRegister8(MPR121_ECR);
+    // スタンバイモードに設定
+    softWire->beginTransmission(_i2caddr);
+    softWire->write(MPR121_ECR);
+    softWire->write(0x00);
+    softWire->endTransmission();
+  }
+  
+  // レジスタに値を書き込む
+  softWire->beginTransmission(_i2caddr);
+  softWire->write(reg);
+  softWire->write(value);
+  softWire->endTransmission();
+  
+  if (stop_required) {
+    // ECRの以前の設定を復元
+    softWire->beginTransmission(_i2caddr);
+    softWire->write(MPR121_ECR);
+    softWire->write(ecr_backup);
+    softWire->endTransmission();
+  }
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,25 @@
 #include <Arduino.h>
 #include <ArduinoLowPower.h>
-#include <Wire.h>
-#include <Adafruit_MPR121.h> // Capacitive touch sensor library
-#include <LSM6DS3.h> // Gyro and accelerometer library
+#include <LSM6DS3.h> // ジャイロスコープとアクセロメータライブラリ
 #include "MatrixButton.h"
+#include "MPR121.h" // ソフトウェアI2C版MPR121を使用
 
 #define READV_PIN D2         // 電圧測定ピン
 #define POWER_BUTTON_PIN D0  // 電源ボタン
 #define LED_PIN D1           // 電源確認LEDピン
 
-#define LSM6DS3_ADDRESS 0x6A // I2C address for LSM6DS3
-#define MPR121_ADDRESS 0x5A // I2C address for MPR121
+#define LSM6DS3_ADDRESS 0x6A // LSM6DS3のI2Cアドレス
+#define MPR121_ADDRESS 0x5A  // MPR121のI2Cアドレス
 
 LSM6DS3 gyroIMU(I2C_MODE, LSM6DS3_ADDRESS);
-Adafruit_MPR121 touchSensor = Adafruit_MPR121();
+//Adafruit_MPR121 touchSensor = Adafruit_MPR121(); // オリジナルライブラリをコメントアウト
+MPR121 touchSensor; // ソフトウェアI2C版MPR121
 MatrixButton matrixButton;
 
 static uint16_t lastTouch = 0;
 static uint16_t nextTouch = 0;
+static unsigned long pressStartTime = 0;
+static bool setDeepSleep = false;
 
 uint8_t readVoltage() {
   int sensorValue = analogRead(READV_PIN);
@@ -52,25 +54,17 @@ bool checkForDeepSleep() {
 void setup() {
   Serial.begin(115200);
   while(!Serial) delay(10);
-
-  delay(50);
-
-  // I2C setup to use speed 400kHz
-  Wire.begin();
-  delay(10);
-  Wire.setClock(400000);
   
   pinMode(READV_PIN, INPUT);
   pinMode(POWER_BUTTON_PIN, INPUT);
 
   matrixButton.begin();
-  
   while(gyroIMU.begin()) {
     Serial.println("Initializing LSM6DS3...");
     delay(500);
   }
 
-  while (!touchSensor.begin(MPR121_ADDRESS)) {
+  while (!touchSensor.begin(MPR121_ADDRESS, SDA, SCL)) {
     Serial.println("Initializing MPR121...");
     delay(500);
   }
@@ -84,11 +78,15 @@ void loop() {
   //matrixButton.scan();
 
   nextTouch = touchSensor.touched();
-  Serial.print("Touch: ");
-  Serial.println(nextTouch, HEX);
+  if (nextTouch != lastTouch) {
+    Serial.print("Touch: ");
+    Serial.println(nextTouch, HEX);
+    lastTouch = nextTouch;
+  }
+  delay(500);
 
-  Serial.print("Gyro AccelX: ");
-  Serial.println(gyroIMU.readFloatAccelX());
+  //Serial.print("Gyro AccelX: ");
+  //Serial.println(gyroIMU.readFloatAccelX());
   
 
   // ジャイロセンサーの値を読み取りマウスとして使用する
@@ -117,7 +115,6 @@ void loop() {
   //}
   
 
-  delay(500);
   //lastTouch = nextTouch;
 
 //  if(checkForDeepSleep()) {
